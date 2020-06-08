fbfa2617d87b66bc7ce5494430f95c9e9de8bcd8
vjpal3 <vrishalipal@yahoo.com>
2020-06-08 18:56:52 -0400

    Add React client code to deploy on Heroku

---
+++ src/main/resources/static/precache-manifest.fe8def3444919bf409c786091ad0b028.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67ca1671edd541381f176d0bce54995",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80989425a36c129fab6e",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6e70756e1f7fb9a60891",
+    "url": "/static/css/main.86ded62d.chunk.css"
+  },
+  {
+    "revision": "80989425a36c129fab6e",
+    "url": "/static/js/2.1275b212.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1275b212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e70756e1f7fb9a60891",
+    "url": "/static/js/main.90f0e60f.chunk.js"
+  },
+  {
+    "revision": "165f034ffb316832c97f",
+    "url": "/static/js/runtime-main.86e74220.js"
+  }
+]);
